bbcec859a4d924e290f322b8fdd264d5b25c5961
4hTp Klasa 4hTp 2022-2023 <4hTp@pracownia308.local>
2023-04-25 16:33:50 +0200

    Pierwszy commit

---
+++ src/components/ClickedButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ClickedButton({handleButtonClick})
+{
+    return(
+        <button onClick={handleButtonClick}>
+            Oblicz
+        </button>
+    )
+}
+
+export default ClickedButton;
